4e5831e44afb5bdd1c821ea2f7df9f453d6dddd8
yhyu13 <yohan680919@163.com>
2024-01-27 02:10:51 +0800

    [Common] Define build macro

---
+++ Engine/Source/Common/Public/GlobalDefinition.h
@@ -0,0 +1,11 @@
+#ifndef HLVM_BUILD_DEBUG
+    #define HLVM_BUILD_DEBUG 0
+#endif
+
+#ifndef HLVM_BUILD_DEVELOPMENT
+    #define HLVM_BUILD_DEVELOPMENT 1
+#endif
+
+#ifndef HLVM_BUILD_RELEASE
+    #define HLVM_BUILD_RELEASE 0
+#endif
