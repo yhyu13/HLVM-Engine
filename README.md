2d4cdfa803ba34ef5660e49295a608bdcaccebcc
yhyu13 <yohan680919@163.com>
2024-02-09 22:05:02 +0800

    [Misc] cleanup repo

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # HLVM-Engine
+
+## Brief
+
+是工作中受UE启发的个人游戏引擎练习项目，在搭建游戏引擎关键基础设施的同时，额外目标是创造比UE5更友好更便捷的pak文件查读和内存管理、遥测系统，同时磨刀linux开发工具链和c++20特性熟练度，不涉及渲染动画特效等一系列游戏内容开发的工作流程，欢迎交流学习
+
+## Build
+
+### Linux-x64
+
+#### Prerequisites:
+
+配置暂时放在zhihu上，有时间再放到项目bundle在一起
+https://zhuanlan.zhihu.com/p/677704467
+
+- Anaconda3
+- git
+- clang-16
+- cmake 3.28
+
+然后
+```
+./Setup.sh
+./GenerateCMakeProjects.sh
+```
--- Setup.sh
+++ Setup.sh
@@ -2,13 +2,30 @@
 
 ROOT_DIR=$(pwd)
 
+# install conda env
+conda env create -f ./env.yaml
+
 # Setup
 git submodule update --init --recursive
 
 # vcpkg
-cd Engine/Source/Dependency/vcpkg
+cd ./Engine/Source/Dependency/vcpkg
 ./bootstrap-vcpkg.sh
-cd $ROOT_DIR
+cd "$ROOT_DIR" || exit
 
-# install conda env
-conda env create -f env.yaml
+# Execute all setup scripts
+find ./Engine/ -name "Dependency" -type d -prune -o -name "Setup.sh" -print | while IFS= read -r file; do
+    # Get the directory of the current file
+    dir=$(dirname "$file")
+
+    # Navigate to that directory
+    cd "$dir" || { echo "Failed to cd into $dir"; continue; }
+
+    # Execute the script with bash (assuming it is a bash script)
+    if ! eval "./$(basename "$file")"; then
+        echo "Script at '$file' failed with an error."
+    fi
+
+    # Return to the original working directory after execution
+    cd "$ROOT_DIR" || exit
+done
