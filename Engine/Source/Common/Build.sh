364e215e02c25e087faf37e9854f67ac4c703eed
yhyu13 <yohan680919@163.com>
2024-03-31 17:51:41 +0800

    [ToolChain] Try extract cmake used binaries into standalone bundle

--- Engine/Source/Common/Build.sh
+++ Engine/Source/Common/Build.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
 time {
-ROOT_DIR=$(pwd)
+
+export ROOT_DIR=$(pwd)
+export REPO_DIR=${ROOT_DIR}/../../..
+
+# 获取环境变量
+source ${REPO_DIR}/Binary/GNULinux-x64/.env
+
+# 定义要构建的目录
+export CMAKE_SRC_DIR=${ROOT_DIR}
+
 # 定义要构建的目标目录
-directories=("./Build/Debug" "./Build/RelWithDebInfo" "./Build/Release")
+buildConfigs=("Debug" "RelWithDebInfo" "Release")
 
-for dir in "${directories[@]}"; do
-    # 进入目标目录并清理旧的构建
-    cd "$dir" || exit 1
-    #rm -rf CMakeFiles/
+for config in "${buildConfigs[@]}"; do
 
-    # 配置CMake（假设项目的顶级目录是上级目录）
-    #cmake -DCMAKE_BUILD_TYPE=$(basename "$dir") ../
+    CMAKE_BUILD_TYPE=${config}
+    CMAKE_BUILD_DIR=${ROOT_DIR}/Build/${CMAKE_BUILD_TYPE}
+
+    # Gen cmake build directory
+    ${CMAKE_BIN} \
+      -Wno-dev \
+      -G Ninja \
+      -S ${CMAKE_SRC_DIR} \
+      -B ${CMAKE_BUILD_DIR} \
+       || exit 1
+
+    # 进入目标目录
+    cd "${CMAKE_BUILD_DIR}" || exit 1
 
     # 构建项目
-    (cmake --build . && ctest . || exit 1) | tee "$ROOT_DIR"/build_"$(basename "$dir")".log
+    (${CMAKE_BIN} --build . && ${CTEST_BIN} . || exit 1) | tee "${ROOT_DIR}/build_${config}.log"
 
     # 回到上级目录
-    cd "$ROOT_DIR" || exit 1
+    cd "${ROOT_DIR}" || exit 1
 done
 
 echo "Finished building all targets"
